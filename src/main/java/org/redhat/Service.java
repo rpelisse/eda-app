2f8b36b9c7055d204b3291acdbac9041e4324a52
Romain Pelisse <belaran@redhat.com>
2024-10-16 19:42:33 +0200

    init

---
+++ src/main/java/org/redhat/Service.java
@@ -0,0 +1,23 @@
+package org.redhat;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("app")
+public class Service {
+    @GET
+    @Path("hello")
+    public String helloworld() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/org/redhat/SimpleRestEndpoint.java
@@ -0,0 +1,21 @@
+package org.redhat;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("tutorial")
+public class SimpleRestEndpoint extends Application {
+}
